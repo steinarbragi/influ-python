c3432ba9d6db51f7e9b4dbc48ee51c1c9769c0b6
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2017-12-11 13:35:06 +0100

    relative paths..

--- draw_figures.py
+++ draw_figures.py
@@ -15,25 +15,20 @@ import numpy as np
 #==============================================================================
 # accuracy of spread prediction
 #==============================================================================
-df1 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_flixster/sample_dataset/prediction/PC_0.txt",
-                   sep=' ', header=None,index_col = None, 
+df1 = pd.read_csv("exp_flixster/PC_0.txt",
+                   sep=' ', header=None,index_col = None,
                    names=['action_id','num_initiators','actual_spread','predicted_spread'])
 actual_spread_1 = df1['actual_spread'].values
 predicted_spread_1 = df1['predicted_spread'].values
 AE1 = np.array(abs(actual_spread_1 - predicted_spread_1),dtype=float)
 
-df2 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_flickr/sample_dataset/prediction/PC_0.txt",
-                  sep=' ', header=None,index_col = None,
-                  names=['action_id','num_initiators','actual_spread','predicted_spread'])
-actual_spread_2 = df2['actual_spread'].values
-predicted_spread_2 = df2['predicted_spread'].values 
-AE2 = np.array(abs(actual_spread_2 - predicted_spread_2),dtype=float)
 
-df3 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_digg/sample_dataset/prediction/PC_0.txt",
+
+df3 = pd.read_csv("exp_digg/PC_0.txt",
                   sep=' ', header=None,index_col = None,
                   names=['action_id','num_initiators','actual_spread','predicted_spread'])
 actual_spread_3 = df3['actual_spread'].values
-predicted_spread_3 = df3['predicted_spread'].values 
+predicted_spread_3 = df3['predicted_spread'].values
 AE3 = np.array(abs(actual_spread_3 - predicted_spread_3),dtype=float)
 
 #### total time taken : 672.417 for digg prediction under 10 data in actions_in_testing.txt
@@ -43,23 +38,14 @@ plt.plot(actual_spread_1, AE1, 'bo')
 plt.xlabel("Actual spread")
 plt.ylabel("Absolute error")
 plt.figure(22)
-plt.plot(actual_spread_2, AE2, 'r*')
-plt.xlabel("Actual spread")
-plt.ylabel("Absolute error")
-plt.figure(23)
 plt.plot(actual_spread_3, AE3, 'gs')
 plt.xlabel("Actual spread")
 plt.ylabel("Absolute error")
-
 plt.figure(31)
 plt.plot(actual_spread_1, predicted_spread_1, 'bo', markersize=2)
 plt.xlabel("Actual spread")
 plt.ylabel("Predicted spread")
 plt.figure(32)
-plt.plot(actual_spread_2, predicted_spread_2, 'r*', markersize=2)
-plt.xlabel("Actual spread")
-plt.ylabel("Predicted spread")
-plt.figure(33)
 plt.plot(actual_spread_3, predicted_spread_3, 'gs', markersize=2)
 plt.xlabel("Actual spread")
 plt.ylabel("Predicted spread")
@@ -67,21 +53,21 @@ plt.ylabel("Predicted spread")
 #==============================================================================
 # spread achieved
 #==============================================================================
-df1 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_flixster/sample_dataset/maxinf_CD/PCCov_0_0.001.txt_PCCov.txt",
-            sep=' ', header=None,index_col = None, 
-            names=['seed_number','g1','g2','influence_sp']) 
+df1 = pd.read_csv("exp_flixster/PCCov_0_0.001.txt_PCCov.txt",
+            sep=' ', header=None,index_col = None,
+            names=['seed_number','g1','g2','influence_sp'])
 seed_number_1 = df1['seed_number'].values
 influence_sp_1 = df1['influence_sp'].values
 
-df2 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_flickr/sample_dataset/maxinf_CD/PCCov_0_0.001.txt_PCCov.txt",
-            sep=' ', header=None,index_col = None, 
-            names=['seed_number','g1','g2','influence_sp']) 
+df2 = pd.read_csv("exp_flickr/PCCov_0_0.001.txt_PCCov.txt",
+            sep=' ', header=None,index_col = None,
+            names=['seed_number','g1','g2','influence_sp'])
 seed_number_2 = df2['seed_number'].values
 influence_sp_2 = df2['influence_sp'].values
 
-df3 = pd.read_csv("/Users/macgx/Desktop/our-project/exp_digg/sample_dataset/maxinf_CD/PCCov_0_0.001.txt_PCCov.txt",
-            sep=' ', header=None,index_col = None, 
-            names=['seed_number','g1','g2','influence_sp']) 
+df3 = pd.read_csv("exp_digg/PCCov_0_0.001.txt_PCCov.txt",
+            sep=' ', header=None,index_col = None,
+            names=['seed_number','g1','g2','influence_sp'])
 seed_number_3 = df3['seed_number'].values
 influence_sp_3 = df3['influence_sp'].values
 
@@ -97,3 +83,4 @@ plt.figure(43)
 plt.plot(seed_number_3, influence_sp_3, 'gs')
 plt.xlabel("Seed set size")
 plt.ylabel("Influence spread achieved")
+plt.show()
